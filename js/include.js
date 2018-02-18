dfd9c876e2f1ea7deeed0c79cfe65b5ba9e4f952
RaunoL <32326803+RaunoL@users.noreply.github.com>
2018-02-18 15:19:00 +0200

    Add files via upload

---
+++ js/include.js
@@ -0,0 +1,13 @@
+function include(path){
+    document.write("<script src="+path+"></script>");
+}
+//lightbox
+include("js/lightbox.js");
+//jquery
+include("js/jquery-3.3.1.min.js");
+//tether
+include("js/tether.min.js");
+//bootstrap
+include("js/bootstrap.bundle.min.js");
+//font-awesome
+include("js/fontawesome-all.min.js");
