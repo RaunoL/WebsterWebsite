c6e1ae851ec4092864522e21b4b0bcdf8ab7f16b
RaunoL <32326803+RaunoL@users.noreply.github.com>
2018-03-06 02:58:49 +0000

    Add files via upload

--- js/order.js
+++ js/order.js
@@ -1,7 +1,128 @@
 var method = '';
 
+function priceCalculation() {
+    var price = '';
+    var MKEE = parseFloat(document.getElementById('quantityEE').value);
+    var MKEN = parseFloat(document.getElementById('quantityEN').value);
+    var quantity = MKEE + MKEN;
+    var transport = '';
+    if (quantity > 1000) {
+        price = quantity * 6.35;
+    } else if (quantity > 500) {
+        price = quantity * 8.63;
+    } else if (quantity > 250) {
+        price = quantity * 9.6;
+    } else if (quantity > 100) {
+        price = quantity * 12;
+    } else if (quantity > 50) {
+        price = quantity * 13.2;
+    } else if (quantity < 1000) {
+        price = quantity * 14.4;
+    }
+    document.getElementById('MKPrice').innerHTML = Math.round(price * 100) / 100 + '€';
+    if (quantity == 1) {
+        transport = 1.26;
+        price += transport
+    }
+    if (quantity == 2 || quantity == 3 || quantity == 4) {
+        transport = 1.46;
+        price += transport
+    }
+    if (quantity >= 5 && quantity <= 10) {
+        transport = 1.84;
+        price += transport
+    }
+    if (quantity >= 11 && quantity <= 20) {
+        transport = 1.90;
+        price += transport
+    }
+    if (quantity >= 21 && quantity <= 30) {
+        transport = 2.10;
+        price += transport
+    }
+    if (quantity >= 31 && quantity <= 59) {
+        transport = 3.54;
+        price += transport
+    }
+    if (quantity >= 60 && quantity <= 72) {
+        transport = 3.7;
+        price += transport
+    }
+    if (method == "omniva") {
+        if (quantity <= 4) {
+            price = price + 2.25;
+            transport = transport + 2.25;
+            document.getElementById("transport").innerHTML = Math.round(transport * 100) / 100 + "€";
+        }
+        if (quantity >= 5 && quantity <= 30) {
+            price = price + 2.89;
+            transport += 2.89;
+            document.getElementById("transport").innerHTML = Math.round(transport * 100) / 100 + "€";
+        }
+        if (quantity >= 31) {
+            price = price + 3.99;
+            transport += 3.99;
+            document.getElementById("transport").innerHTML = Math.round(transport * 100) / 100 + "€";
+        }
+
+    }
+    if (method == "smartpost") {
+        if (quantity <= 4) {
+            price = price + 2.35;
+            transport = transport + 2.35;
+            document.getElementById("transport").innerHTML = Math.round(transport * 100) / 100 + "€";
+        }
+        if (quantity >= 5 && quantity <= 30) {
+            price = price + 3.02;
+            transport = transport + 3.02;
+            document.getElementById("transport").innerHTML = Math.round(transport * 100) / 100 + "€";
+        }
+        if (quantity >= 31) {
+            price = price + 4.05;
+            transport += 4.05;
+            document.getElementById("transport").innerHTML = Math.round(transport * 100) / 100 + "€";
+        }
+    }
+    if (method == "EEkuller") {
+        if (quantity <= 20) {
+            price = price + 2.1;
+            transport += 2.1;
+            document.getElementById("transport").innerHTML = Math.round(transport * 100) / 100 + "€";
+        }
+        if (quantity >= 21 && quantity <= 80) {
+            price = price + 3;
+            transport += 3;
+            document.getElementById("transport").innerHTML = Math.round(transport * 100) / 100 + "€";
+        }
+        if (quantity >= 81) {
+            price = price + 3.5;
+            transport += 3.5;
+            document.getElementById("transport").innerHTML = Math.round(transport * 100) / 100 + "€";
+        }
+    }
+    if (method == "EUkuller") {
+        if (quantity == 1) {
+            price = price + 6.2;
+            transport += 6.2;
+            document.getElementById("transport").innerHTML = Math.round(transport * 100) / 100 + "€";
+        }
+        if (quantity == 2 || quantity == 3) {
+            price = price + 10;
+            transport += 10;
+            document.getElementById("transport").innerHTML = Math.round(transport * 100) / 100 + "€";
+        }
+        if (quantity >= 4 && quantity <= 6) {
+            price = price + 16.8;
+            transport += 16.8;
+            document.getElementById("transport").innerHTML = Math.round(transport * 100) / 100 + "€";
+        }
+    }
+    document.getElementById('sum').innerHTML = Math.round(price * 100) / 100 + '€';
+
+}
+
 function deliver(option) {
-    console.log(option);
+
     document.getElementById('delivery-options').style.display = 'none';
     document.getElementById('smartpost').style.display = 'none';
     document.getElementById('omniva').style.display = 'none';
@@ -9,6 +130,8 @@ function deliver(option) {
     document.getElementById('EUkuller').style.display = 'none';
     document.getElementById(option).style.display = 'block';
     method = option;
+    console.log(option);
+    priceCalculation();
 }
 
 function validate() {
@@ -21,6 +144,7 @@ function validate() {
     var phone = document.getElementsByName('phone')[0].value;
     var quantityEE = document.getElementById('quantityEE').value;
     var quantityEN = document.getElementById('quantityEN').value;
+    document.getElementById('validation-error').style.display = 'block';
     if (firstname == '') {
         document.getElementById('firstname').style.display = 'block';
         return;
@@ -128,6 +252,7 @@ function validate() {
     } else {
         document.getElementById('quantity-error').style.display = 'none';
     }
+    document.getElementById('validation-error').style.display = 'none';
     window.location.href = '/confirm.html?' + 'firstname=' +
         firstname + '&lastname=' +
         lastname + '&company=' +
@@ -135,7 +260,9 @@ function validate() {
         email + '&phone=' +
         phone + '&method=' +
         method + '&address=' +
-        address;
+        address + '&quantityEE=' +
+        quantityEE + '&quantityEN=' +
+        quantityEN;
 
 
 
